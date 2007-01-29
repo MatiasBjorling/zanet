e21adaea46fa924327c486c6951e965bf426f705
mbjoerling <mbjoerling@f59a7d9a-1320-0410-97a2-8ef4687d0339>
2007-01-29 11:39:36 +0000

    git-svn-id: svn://svn.code.sf.net/p/zanet/code@65 f59a7d9a-1320-0410-97a2-8ef4687d0339

--- zabbix/SubSystem/update.bat
+++
@@ -1,7 +0,0 @@
-net stop zabbixagentnet
-
-del "%Apppath%\ZabbixAgent_old.exe"
-move "%Apppath%\ZabbixAgent.exe" "%Apppath%\ZabbixAgent_old.exe"
-move "%Apppath%\ZabbixAgent_new.exe" "%Apppath%\ZabbixAgent.exe"
-
-net start zabbixagentnet
